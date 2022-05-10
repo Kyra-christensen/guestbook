111026a102bb732d52833418fd4411dc244ec7fc
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-10 02:32:59 -0700

    completed header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { useUser } from '../context/UserContext';
+import { signOutUser } from '../services/user';
 
 export default function Header() {
+  const { user, logout } = useUser();
+
+  async function handleSignOut() {
+    setUser('');
+
+    await signOutUser();
+  }
+
   return (
-    <div>Header</div>
+    <>
+      <h1>Guestbook</h1>
+      {user.email
+        ? <>
+            <h4>logged in as {user.email}</h4>
+            <button onClick={logout}>Sign Out</button>
+          </>
+        : <h4>Sign Up/In</h4>
+      }
+    </>
   )
 }

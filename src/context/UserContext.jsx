66e6238eba0c3de270112194c20633e96b6d1ff6
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-10 01:47:39 -0700

    created functions in UserContext.jsx

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { createContext, useContext, useState } from 'react';
+import { getUser, signInUser, signUpUser } from '../services/user';
 
-export default function UserContext() {
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser || { email: null });
+
+  const login = async (email, password) => {
+    const authenticatedUser = await signInUser({ email, password });
+
+    if (authenticatedUser) {
+      setUser(authenticatedUser)
+    }
+  };
+
+  const signUp = async (email, password) => {
+    const authenticatedUser = await signUpUser({ email, password });
+
+    if (authenticatedUser) {
+      setUser(authenticatedUser)
+    };
+
+  };
+
+  const logout = () => {
+    setUser({ email: null });
+  };
+
   return (
-    <div>UserContext</div>
-  )
+    <UserContext.Provider value={{ user, login, signUp, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('You must use useUser within context');
+  }
+
+  return context;
 }

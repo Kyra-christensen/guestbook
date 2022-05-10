886fa74777f9b1735a2c6040df6573b6b55447e0
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-10 03:17:21 -0700

    created functions in entries.js, created form in entryform.jsx, and created functions in dashboard.jsx

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import EntryForm from '../../components/EntryForm';
+import { useUser } from '../../context/UserContext';
+import { getEntries } from '../../services/entries';
 
 export default function Dashboard() {
+  const { user } = useUser();
+  const [entries, setEntries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log(entries);
+
+  useEffect(() => {
+    getEntries()
+      .then(setEntries)
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
+
+  async function refreshEntries() {
+    setLoading(true);
+    getEntries()
+    .then(setEntries)
+    .catch(console.error)
+    .finally(() => setLoading(false));
+  }
+
   return (
-    <div>Dashboard</div>
+    <>
+      <h2>Dashboard</h2>
+      <EntryForm refreshEntries={refreshEntries}/>
+      {loading
+        ? <p>Loading entries</p>
+        : <ul>{entries.map((entry) => (
+          <li key={entry.id}>{entry.content} <br />
+            author: {user.email} </li>
+        ))}</ul>
+      }
+    </>
   )
 }
